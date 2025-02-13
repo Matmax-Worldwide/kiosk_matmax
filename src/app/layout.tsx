f7bfefb826e79a357b6e5bd2c6fada48f32276d9
Rodvdev <rodrigovdev01@gmail.com>
2025-02-13 16:16:32 -0500

    header padding

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
           <LanguageProvider>
             <NotificationProvider>
               <AnimatedLayout>
-                <div className="absolute top-0 w-full h-screen">
+                <div className="absolute top-0 w-full h-screen pt-16">
                   {children}
                 </div>
               </AnimatedLayout>
